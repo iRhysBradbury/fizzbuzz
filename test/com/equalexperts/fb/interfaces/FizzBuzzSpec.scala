a6eb34a027f051f221f1f5e3811bc3c61bb75d26
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-08 19:44:06 +0000

    formatting test name

--- test/com/equalexperts/fb/interfaces/FizzBuzzSpec.scala
+++ test/com/equalexperts/fb/interfaces/FizzBuzzSpec.scala
@@ -7,7 +7,11 @@ object FizzBuzzTest extends FizzBuzz
 
 class FizzBuzzSpec extends FlatSpec with Matchers {
 
-  it should "produce the correct fizz buzz sequence for 1 to 20" in {
+  it should "produce\n" +
+    "the number\n" +
+    "‘fizz’ for numbers that are multiples of 3\n" +
+    "‘buzz’ for numbers that are multiples of 5\n" +
+    "‘fizzbuzz’ for numbers that are multiples of 15" in {
     val expected = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz"
     val actual = FizzBuzzTest.fizzBuzz(
       start = 1,
