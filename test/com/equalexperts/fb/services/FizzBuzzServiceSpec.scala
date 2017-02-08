7c5ac9c782102d0cc2e33a55da51f5c8d9e964c5
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-08 19:46:17 +0000

    adding service test

--- test/com/equalexperts/fb/services/FizzBuzzServiceSpec.scala
+++ test/com/equalexperts/fb/services/FizzBuzzServiceSpec.scala
@@ -1,9 +1,13 @@
 package com.equalexperts.fb.services
 
-import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.{OneAppPerTest, PlaySpec}
 
-class FizzBuzzServiceSpec extends PlaySpec {
+class FizzBuzzServiceSpec extends PlaySpec with OneAppPerTest {
 
-
-
+  "FizzBuzzService" should {
+    "be injectable" in {
+      app.injector.instanceOf[FizzBuzzService]
+      assert(true)
+    }
+  }
 }
