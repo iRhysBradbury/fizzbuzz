7cb62f535fa71e3774ed6f6e85c4af1c9f60b279
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-08 20:37:34 +0000

    task 3

--- app/com/equalexperts/fb/interfaces/FizzBuzz.scala
+++ app/com/equalexperts/fb/interfaces/FizzBuzz.scala
@@ -6,12 +6,25 @@ package com.equalexperts.fb.interfaces
 trait FizzBuzz {
 
   /**
+    * Terms to for output.
+    */
+  object Terms {
+    val Lucky = "lucky"
+    val Fizzbuzz = "fizzbuzz"
+    val Buzz = "buzz"
+    val Fizz = "fizz"
+    val Integer = "integer"
+    val Delimiter = " "
+  }
+
+  /**
     * This implicit class `extends` Int type to allow
     * `isMultipleOf` functions on `Int` type.
     *
     * @param i the integer comparable
     */
   implicit class IntExtension(i: Int) {
+
     /**
       * Returns `true` if the number is a multiple of `m`.
       * Returns `false` if the number is not a multiple of `m`.
@@ -36,27 +49,64 @@ trait FizzBuzz {
   }
 
   /**
-    * This method contains the logic for the fizz buzz `sequence`.
+    * This method contains the logic for the FizzBuzz `sequence`.
     *
     * @param start the start of the range
     * @param end the end of the range
-    * @return a `String` which is representative of the fizzbuzz sequence
+    * @return a `String` which is representative of the FizzBuzz sequence
     */
-  def fizzBuzz(start: Int, end: Int): String = {
+  def fizzBuzzSequence(start: Int, end: Int): String = {
     {
       start.to(end).map { i =>
         if(i.contains(3)) {
-          "lucky"
+          Terms.Lucky
         } else if(i.isMultipleOf(15)) {
-          "fizzbuzz"
+          Terms.Fizzbuzz
         } else if(i.isMultipleOf(5)) {
-          "buzz"
+          Terms.Buzz
         } else if(i.isMultipleOf(3)) {
-          "fizz"
+          Terms.Fizz
         } else {
           i.toString
         }
       }
-    }.mkString(" ")
+    }.mkString(Terms.Delimiter)
+  }
+
+  /**
+    * Produces a report on a `String` about how many FizzBuzz terms are used.
+    *
+    * @param fizzBuzzStr the FizzBuzz string
+    * @return the original
+    */
+  def fizzBuzzReport(fizzBuzzStr: String): String = {
+    val terms = fizzBuzzStr.split(Terms.Delimiter)
+
+    val fizzCount = terms.count(_ == Terms.Fizz)
+    val buzzCount = terms.count(_ == Terms.Buzz)
+    val fizzbuzzCount = terms.count(_ == Terms.Fizzbuzz)
+    val luckyCount = terms.count(_ == Terms.Lucky)
+    val intCount = terms.size - fizzCount - buzzCount - fizzbuzzCount - luckyCount
+    s"${Terms.Fizz}: $fizzCount " +
+    s"${Terms.Buzz}: $buzzCount " +
+    s"${Terms.Fizzbuzz}: $fizzbuzzCount " +
+    s"${Terms.Lucky}: $luckyCount " +
+    s"${Terms.Integer}: $intCount"
+  }
+
+  /**
+    * This method contains the logic for the FizzBuzz `sequence` and FizzBuzz report.
+    *
+    * @param start start of the FizzBuzz `sequence`
+    * @param end end of the FizzBuzz `sequence`
+    * @return
+    */
+  def fizzBuzz(start: Int, end: Int): String = {
+    val str = fizzBuzzSequence(
+      start = start,
+      end = end
+    )
+    val report = fizzBuzzReport(str)
+    s"$str $report"
   }
 }
--- test/com/equalexperts/fb/interfaces/FizzBuzzSpec.scala
+++ test/com/equalexperts/fb/interfaces/FizzBuzzSpec.scala
@@ -13,6 +13,27 @@ class FizzBuzzSpec extends FlatSpec with Matchers {
     "‘buzz’ for numbers that are multiples of 5\n" +
     "‘fizzbuzz’ for numbers that are multiples of 15" in {
     val expected = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz"
+    val actual = FizzBuzzTest.fizzBuzzSequence(
+      start = 1,
+      end = 20
+    )
+    actual shouldBe expected
+  }
+
+  it should "produce a fizzbuzz report" in {
+    val expected = "fizz: 1 buzz: 1 fizzbuzz: 1 lucky: 1 integer: 1"
+    val actual = FizzBuzzTest.fizzBuzzReport("fizz buzz fizzbuzz lucky 2")
+    actual shouldBe expected
+  }
+
+  it should "produce\n" +
+    "a report on count of\n" +
+    "fizz\n" +
+    "buzz\n" +
+    "fizzbuzz\n" +
+    "lucky\n" +
+    "an integer" in {
+    val expected = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz fizz: 4 buzz: 3 fizzbuzz: 1 lucky: 2 integer: 10"
     val actual = FizzBuzzTest.fizzBuzz(
       start = 1,
       end = 20
